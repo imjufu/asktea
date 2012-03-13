53587177db77babb45c2f3bd6800f34fb45a6655
Mikael RANDY <mikael.randy@gmail.com>
2012-03-13 15:31:36 +0100

    Add author as text for question and comment

--- data/sql/data.sql
+++ data/sql/data.sql
@@ -3,23 +3,23 @@
 -- #######
 DELETE FROM Question;
 
-INSERT INTO "Question" VALUES(1,'Est-ce une blague ?','Est-ce que vous pensez réussir ?','2012-03-13');
-INSERT INTO "Question" VALUES(2,'Comment acheter','Je veux acheter chez vous','2012-03-13');
-INSERT INTO "Question" VALUES(3,'Est-ce que tout sera open source ?','Est-ce que tout sera open-source ?','2012-03-13');
+INSERT INTO "Question" VALUES(1,'mrandy', 'Est-ce une blague ?','Est-ce que vous pensez réussir ?','2012-03-13');
+INSERT INTO "Question" VALUES(2,'jfusco','Comment acheter','Je veux acheter chez vous','2012-03-13');
+INSERT INTO "Question" VALUES(3,'edaspet', 'Est-ce que tout sera open source ?','Est-ce que tout sera open-source ?','2012-03-13');
 
 -- #######
 -- ## Comments
 -- #######
-DELETE Comment;
+DELETE FROM Comment;
 
-INSERT INTO "Comment" VALUES(1,1,'Commentaire 1','2012-03-13');
-INSERT INTO "Comment" VALUES(2,1,'Commentaire 2','2012-03-13');
-INSERT INTO "Comment" VALUES(3,2,'commentaire 3','2012-03-13');
+INSERT INTO "Comment" VALUES(1,1,'edaspet','Commentaire 1','2012-03-13');
+INSERT INTO "Comment" VALUES(2,1,'mrandy','Commentaire 2','2012-03-13');
+INSERT INTO "Comment" VALUES(3,2,'jfusco','commentaire 3','2012-03-13');
 
 -- #######
 -- ## Votes
 -- #######
-DELETE Vote;
+DELETE FROM Vote;
 
 INSERT INTO "Vote" VALUES(1,1,'192.168.1.20','2012-03-13');
 INSERT INTO "Vote" VALUES(2,1,'192.168.1.21','2012-03-13');
