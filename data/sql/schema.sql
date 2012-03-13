912841270b3b0481b34425959c73e6fa5b57f27a
Mikael RANDY <mikael.randy@gmail.com>
2012-03-13 12:20:56 +0100

    Improve Question and Comment table and add Vote table

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -1,24 +1,33 @@
 -- #######
 -- ## Question table
 -- #######
-CREATE TABLE IF NOT EXISTS `Question` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT ,
-  `body` VARCHAR(45) NOT NULL
+DROP TABLE Question;
+
+CREATE TABLE IF NOT EXISTS Question (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  creation_date DATETIME NOT NULL
 );
 
 
 -- #######
 -- ## Answer table
 -- #######
+DROP TABLE Comment;
+
 CREATE TABLE IF NOT EXISTS Comment (
   id INTEGER PRIMARY KEY AUTOINCREMENT ,
   question_id INTEGER NOT NULL,
-  body VARCHAR(45) NOT NULL
+  body TEXT NOT NULL,
+  creation_date DATETIME NOT NULL
 );
 
 -- #######
 -- ##  Vote table
 -- #######
+DROP TABLE Vote;
+
 CREATE TABLE IF NOT EXISTS Vote (
 	id INTEGER PRIMARY KEY NOT NULL,
 	question_id INTEGER NOT NULL,
