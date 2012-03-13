4d88f93592967b8b2fb1b24d3c66dd240fdf4c02
Mikael RANDY <mikael.randy@gmail.com>
2012-03-13 09:25:52 +0100

    Improve database schema to handle vote

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -1,9 +1,27 @@
+-- #######
+-- ## Question table
+-- #######
 CREATE TABLE IF NOT EXISTS `Question` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT ,
-  `value` VARCHAR(45) NOT NULL
+  `body` VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `Answer` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT ,
-  `value` VARCHAR(45) NOT NULL
+
+-- #######
+-- ## Answer table
+-- #######
+CREATE TABLE IF NOT EXISTS Comment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT ,
+  question_id INTEGER NOT NULL,
+  body VARCHAR(45) NOT NULL
+);
+
+-- #######
+-- ##  Vote table
+-- #######
+CREATE TABLE IF NOT EXISTS Vote (
+	id INTEGER PRIMARY KEY NOT NULL,
+	question_id INTEGER NOT NULL,
+	ip VARCHAR(255) NOT NULL,
+	creation_date DATETIME NOT NULL
 );
