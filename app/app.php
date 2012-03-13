77e3d07907b5f2b2521ddaeebc09f68cafd61dd4
Mikael RANDY <mikael.randy@gmail.com>
2012-03-13 12:23:42 +0100

    Question controller creation   Display a question

--- app/app.php
+++ app/app.php
@@ -3,5 +3,6 @@
 $app = require_once __DIR__.'/bootstrap.php';
 
 $app->mount('/', new Asktea\Provider\Controller\Common());
+$app->mount('/question', new Asktea\Provider\Controller\Question());
 
 return $app;
