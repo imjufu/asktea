695552945f13674c2c1c167edb92e76a1deca75c
Mikael RANDY <mikael.randy@gmail.com>
2012-03-13 16:35:25 +0100

    Vote controller creation

--- app/app.php
+++ app/app.php
@@ -4,5 +4,6 @@ $app = require_once __DIR__.'/bootstrap.php';
 
 $app->mount('/', new Asktea\Provider\Controller\Common());
 $app->mount('/question', new Asktea\Provider\Controller\Question());
+$app->mount('/vote', new Asktea\Provider\Controller\Vote());
 
 return $app;
